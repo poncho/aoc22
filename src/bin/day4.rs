51448d9f014b050671ce4703f83699eae3199942
Poncho <poncho.811@gmail.com>
2022-12-05 19:32:47 -0600

    Day 4: Part 2

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -5,8 +5,10 @@ type Day4 = Vec<([u16; 4])>;
 fn main() {
     let input = read_input("inputs/day4.txt");
     let p1_result = puzzle1(&input);
+    let p2_result = puzzle2(&input);
 
     println!("Puzzle #1: {}", p1_result);
+    println!("Puzzle #2: {}", p2_result);
 }
 
 fn read_input(path: &str) -> Day4 {
@@ -40,6 +42,16 @@ fn puzzle1(input: &Day4) -> usize {
         .count()
 }
 
+fn puzzle2(input: &Day4) -> usize {
+    input
+        .iter()
+        .filter(|[r1_start, r1_end, r2_start, r2_end]| {
+            (r1_start <= r2_start && r2_start <= r1_end)
+                || (r2_start <= r1_start && r1_start <= r2_end)
+        })
+        .count()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -53,5 +65,12 @@ mod tests {
         let test_input = test_input();
 
         assert_eq!(puzzle1(&test_input), 2)
+    }
+
+    #[test]
+    fn puzzle2_test() {
+        let test_input = test_input();
+
+        assert_eq!(puzzle2(&test_input), 4)
     }
 }
